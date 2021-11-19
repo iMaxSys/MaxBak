f6f1d95f36f6cdf70da2f842b3c2386d7d6c8693
taojy <taox@live.com>
2021-11-19 17:47:50 +0800

    update tree

--- src/iMaxSys.Max/Algorithm/Collection/Tree.cs
+++ src/iMaxSys.Max/Algorithm/Collection/Tree.cs
@@ -36,6 +36,11 @@ namespace iMaxSys.Max.Algorithm.Collection
         public bool IsLeaf { get; set; }
 
         /// <summary>
+        /// Deep
+        /// </summary>
+        public int Deep { get; set; }
+
+        /// <summary>
         /// Code
         /// </summary>
         public string Code { get; set; }
@@ -54,6 +59,11 @@ namespace iMaxSys.Max.Algorithm.Collection
         /// Icon
         /// </summary>
         public string? Icon { get; set; }
+
+        /// <summary>
+        /// SelectedIcon
+        /// </summary>
+        public string? SelectedIcon { get; set; }
 
         /// <summary>
         /// Style
@@ -81,7 +91,12 @@ namespace iMaxSys.Max.Algorithm.Collection
         public int Status { get; set; }
 
         /// <summary>
-        /// Node collection
+        /// Parent
+        /// </summary>
+        public ITreeNode? Parent { get; set; }
+
+        /// <summary>
+        /// node collection
         /// </summary>
         public IList<ITreeNode>? Nodes { get; set; }
     }
@@ -99,12 +114,17 @@ namespace iMaxSys.Max.Algorithm.Collection
         /// <summary>
         /// Index
         /// </summary>
-        public int Index { get; set; }
+        public int Index { get; set; } = 0;
 
         /// <summary>
         /// IsLeaf
         /// </summary>
-        public bool IsLeaf { get; set; }
+        public bool IsLeaf { get; set; } = true;
+
+        /// <summary>
+        /// Deep
+        /// </summary>
+        public int Deep { get; set; } = 0;
 
         /// <summary>
         /// Code
@@ -125,6 +145,11 @@ namespace iMaxSys.Max.Algorithm.Collection
         /// Icon
         /// </summary>
         public string? Icon { get; set; }
+
+        /// <summary>
+        /// SelectedIcon
+        /// </summary>
+        public string? SelectedIcon { get; set; }
 
         /// <summary>
         /// Style
@@ -150,6 +175,11 @@ namespace iMaxSys.Max.Algorithm.Collection
         /// Status
         /// </summary>
         public int Status { get; set; } = 1;
+
+        /// <summary>
+        /// Parent
+        /// </summary>
+        public ITreeNode? Parent { get; set; }
 
         /// <summary>
         /// node collection
@@ -196,6 +226,11 @@ namespace iMaxSys.Max.Algorithm.Collection
         /// Icon
         /// </summary>
         public string? Icon { get; set; }
+
+        /// <summary>
+        /// SelectedIcon
+        /// </summary>
+        public string? SelectedIcon { get; set; }
 
         /// <summary>
         /// Style
@@ -279,6 +314,11 @@ namespace iMaxSys.Max.Algorithm.Collection
         public string? Icon { get; set; }
 
         /// <summary>
+        /// SelectedIcon
+        /// </summary>
+        public string? SelectedIcon { get; set; }
+
+        /// <summary>
         /// Style
         /// </summary>
         public string? Style { get; set; }
@@ -326,45 +366,52 @@ namespace iMaxSys.Max.Algorithm.Collection
     /// </summary>
     public class Tree
     {
+        const string TAG_ROOT = "root";
+
         /// <summary>
-        /// 根
+        /// root
         /// </summary>
-        public ITreeNode? Root { get; set; }
+        private ITreeNode? _root;
+
+        /// <summary>
+        /// 节点集(平面集)
+        /// </summary>
+        private IList<ITreeNode>? _nodeSet;
+
+        /// <summary>
+        /// 根/树结构
+        /// </summary>
+        public ITreeNode? Root { get => _root; }
 
         /// <summary>
         /// 深度
         /// </summary>
-        public int Deep { get; set; } = 0;
+        public int Deep { get; } = 0;
 
         /// <summary>
         /// 索引数
         /// </summary>
-        public int IndexCount { get; set; } = 0;
+        public int IndexCount { get; } = 0;
 
         /// <summary>
         /// 节点数
         /// </summary>
-        public int NodeCount { get; set; } = 0;
+        public int NodeCount { get; } = 0;
 
         /// <summary>
         /// 叶节点数
         /// </summary>
-        public int LeafCount { get; set; } = 0;
+        public int LeafCount { get; } = 0;
 
         /// <summary>
-        /// 节点集
+        /// 节点集(平面集)
         /// </summary>
-        public IEnumerable<ITreeNode>? NodeSet { get { return _nodeSet ??= GetAll(); } }
+        public IList<ITreeNode>? NodeSet { get => _nodeSet ??= GetAllNodes(); }
 
         /// <summary>
-        /// 节点存储
+        /// 节点存储(平面存储)
         /// </summary>
         public IList<ITreeStore>? Stores { get; }
-
-        /// <summary>
-        /// 节点集
-        /// </summary>
-        private IEnumerable<ITreeNode>? _nodeSet;
 
         /// <summary>
         /// 构造
@@ -375,7 +422,7 @@ namespace iMaxSys.Max.Algorithm.Collection
             if (stores.Count > 0)
             {
                 Stores = stores;
-                Root = GetRoot(stores);
+                _root = GetRoot(stores);
                 IndexCount = stores.Count;
             }
         }
@@ -388,7 +435,7 @@ namespace iMaxSys.Max.Algorithm.Collection
         {
             if (root.Nodes?.Count > 0)
             {
-                Root = root;
+                _root = root;
                 Stores = GetStore(root);
                 IndexCount = Stores.Count;
             }
@@ -405,8 +452,8 @@ namespace iMaxSys.Max.Algorithm.Collection
             var store = stores.FirstOrDefault(x => x.Lv == 0);
             if (store != null)
             {
-                Root = MakeNode(store);
-                SetNodes(stores, Root);
+                _root = MakeNode(store);
+                SetNodes(stores, _root);
                 return Root;
             }
             else
@@ -427,14 +474,19 @@ namespace iMaxSys.Max.Algorithm.Collection
 
             if (current != null)
             {
+                //获取所有子节点
                 var list = stores.Where(x => x.Deep == current.Deep + 1 && x.Lv > current.Lv && x.Rv < current.Rv).OrderBy(x => x.Lv);
-
-                foreach (var store in list)
+                if (list != null && list.Any())
                 {
                     node.Nodes = new List<ITreeNode>();
-                    ITreeNode n = MakeNode(store);
-                    SetNodes(stores, n);
-                    node.Nodes.Add(n);
+                    foreach (var store in list)
+                    {
+                        ITreeNode n = MakeNode(store);
+                        n.Parent = node;
+                        n.Deep = node.Deep + 1;
+                        SetNodes(stores, n);
+                        node.Nodes.Add(n);
+                    }
                 }
             }
         }
@@ -455,6 +507,7 @@ namespace iMaxSys.Max.Algorithm.Collection
                 Name = store.Name,
                 Description = store.Description,
                 Icon = store.Icon,
+                SelectedIcon = store.SelectedIcon,
                 Style = store.Style,
                 Data = store.Data,
                 Action = store.Action,
@@ -479,6 +532,7 @@ namespace iMaxSys.Max.Algorithm.Collection
                 Name = node.Name,
                 Description = node.Description,
                 Icon = node.Icon,
+                SelectedIcon = node.SelectedIcon,
                 Style = node.Style,
                 Data = node.Data,
                 Action = node.Action,
@@ -531,62 +585,137 @@ namespace iMaxSys.Max.Algorithm.Collection
         }
 
         /// <summary>
-        /// 获取所有节点
+        /// IList<ITreeNode>
         /// </summary>
-        /// <returns></returns>
-        public IEnumerable<ITreeNode>? GetAll()
+        private IList<ITreeNode>? GetAllNodes()
         {
-            return Root?.Nodes != null && Root!.Nodes.Count > 0 ? GetAll(Root!.Nodes) : null;
+            SetAllNodes();
+            return _nodeSet;
         }
 
         /// <summary>
-        /// 获取所有节点
+        /// 设置所有节点
         /// </summary>
-        /// <param name="nodes"></param>
         /// <returns></returns>
-        private IEnumerable<ITreeNode> GetAll(IEnumerable<ITreeNode> nodes)
+        private void SetAllNodes()
         {
-            IEnumerable<ITreeNode> list = new List<ITreeNode>();
-
-            foreach (var item in nodes)
+            if (Root?.Nodes != null && Root!.Nodes.Count > 0)
             {
-                list = list.Append(item);
+                _nodeSet = new List<ITreeNode>();
+                CopyNodes(Root.Nodes, _nodeSet);
+            }
+        }
+
+        /// <summary>
+        /// 设置所有节点
+        /// 传参和Add是基于性能考虑
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        private void CopyNodes(IList<ITreeNode> source, IList<ITreeNode> target)
+        {
+            foreach (var item in source)
+            {
+                target.Add(item);
                 if (item.Nodes != null && item.Nodes.Any())
                 {
-                    list = list.Concat(GetAll(item.Nodes));
+                    CopyNodes(item.Nodes, target);
                 }
-                //加完直接点后是否要清楚nodes属性
-                //item.Nodes.Clear();
             }
-            return list;
         }
 
         /// <summary>
-        /// 新增节点
+        /// 新增同级节点(id=0为增加第一级节点)
         /// </summary>
         /// <param name="index">索引位置</param>
         /// <param name="treeNode">节点</param>
-        public void Add(int index, ITreeNode treeNode)
+        public void Add(long id, ITreeNode treeNode)
         {
-
+            //获取目标节点
+            var node = _nodeSet?.FirstOrDefault(x => x.Id == id);
+            if (node != null)
+            {
+                //操作tree, 先找到父节点, 再获取子集插入
+                if (node.Parent != null)
+                {
+                    treeNode.Deep = node.Deep;
+                    treeNode.Parent = node.Parent;
+                    node.Parent.Nodes!.Insert(node.Parent.Nodes!.IndexOf(node) + 1, treeNode);
+                }
+                else
+                {
+                    throw new Exception($"节点:{id}无父节点");
+                }
+            }
+            else
+            {
+                throw new Exception($"无效的目标节点:{id}");
+            }
         }
+
+
 
         /// <summary>
         /// 新增子节点
         /// </summary>
-        /// <param name="index"></param>
+        /// <param name="id"></param>
         /// <param name="treeNode"></param>
-        public void AddSub(int index, ITreeNode treeNode)
+        public void AddSub(long id, ITreeNode treeNode)
         {
+            if (id > 0)
+            {
+                //获取目标节点
+                var node = _nodeSet?.FirstOrDefault(x => x.Id == id);
+                if (node != null)
+                {
+                    treeNode.Deep = node.Deep + 1;
+                    treeNode.Parent = node;
+
+                    node.Nodes ??= new List<ITreeNode>();
+                    node.Nodes.Add(treeNode);
+                }
+                else
+                {
+                    throw new Exception($"无法新增子节点, 无效的目标节点:{id}");
+                }
+            }
+            else
+            {
+                _root ??= new TreeNode
+                {
+                    Name = TAG_ROOT,
+                    Nodes = new List<ITreeNode>()
+                };
+
+                treeNode.Deep = _root.Deep + 1;
+                treeNode.Parent = _root;
+                _root.Nodes = new List<ITreeNode> { treeNode };
+            }
         }
 
         /// <summary>
-        /// 移除节点
+        /// 移除节点(包含子节点无法删除)
         /// </summary>
         /// <param name="index"></param>
-        public void Remove(int index)
+        public void Remove(long id)
         {
-
+            //获取目标节点
+            var node = _nodeSet?.FirstOrDefault(x => x.Id == id);
+            if (node != null)
+            {
+                if (node.Nodes?.Count > 0)
+                {
+                    throw new Exception($"无法删除! 目标节点:{id}下还有子节点.");
+                }
+                else
+                {
+                    node.Parent!.Nodes!.Remove(node);
+                }
+            }
+            else
+            {
+                throw new Exception($"无法删除! 无效的目标节点:{id}.");
+            }
         }
 
         /// <summary>
