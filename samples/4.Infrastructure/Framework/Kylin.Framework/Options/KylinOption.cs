df093642c013e9aa531bb5e23ac9626985018b9d
taojy <taox@live.com>
2022-10-26 20:07:52 +0800

    framwork

--- samples/4.Infrastructure/Framework/Kylin.Framework/Options/KylinOption.cs
+++ samples/4.Infrastructure/Framework/Kylin.Framework/Options/KylinOption.cs
@@ -1,11 +1,23 @@
 ﻿using System;
-namespace Kylin.Framework.Options
-{
-	public class KylinOption
-	{
-		public KylinOption()
-		{
-		}
-	}
-}
 
+using iMaxSys.Max.Options;
+
+namespace Kylin.Framework.Options;
+
+public class KylinOption
+{
+    /// <summary>
+    /// 启用
+    /// </summary>
+    public bool Enable { get; set; }
+
+    /// <summary>
+    /// 版本
+    /// </summary>
+    public string Version { get; set; } = "0.0.0";
+
+    /// <summary>
+    /// 数据库配置集
+    /// </summary>
+    public List<DatabaseOption>? Databases { get; set; }
+}
